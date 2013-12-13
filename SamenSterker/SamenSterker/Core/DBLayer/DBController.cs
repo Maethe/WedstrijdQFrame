857ea767e4ac4bf672b3d924a23d53af28de6431
Maethe <maethe.desmet@student.ap.be>
2013-12-13 11:34:04 +0100

    VS doet weer ambetant

--- SamenSterker/SamenSterker/Core/DBLayer/DBController.cs
+++ SamenSterker/SamenSterker/Core/DBLayer/DBController.cs
@@ -14,6 +14,7 @@ namespace SamenSterker.Core.DBLayer
         public Company getCompanyById(int id)
         {
             DataSet companies = conn.GetTable("Company");
+            fuck this shit
         }
     }
 }
